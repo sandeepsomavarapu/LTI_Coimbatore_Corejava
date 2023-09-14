01a15fa724d5f20541d9f7efc93867c325ae4be5
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2023-09-14 17:54:08 +0530

    Day-1

---
+++ Welcome.java
@@ -0,0 +1,23 @@
+class Welcome123
+{
+	long contact=9494949494l;
+	short marks=890;//global variables 
+	public static void main(String args[])
+	{
+		//local variabels
+		byte age=78;
+		char gender='M';
+		float salary=23.2313213123444f;
+		double result=43.3232323232993;
+		
+		System.out.println(result+"Leave US Its break time"+salary);
+	}
+
+}
+
+//java-->source code 
+//JVM -->binary code/byte code  /machine understandable language 
+//compailation->compiler-->javac filename.java  -->.class
+//.class-->JVM java classname
+
+//a-z A-z 0-9 _ $
